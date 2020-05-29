66acb587d08505f63d00cb3b92fe98be4d41d99d
Ben <32benhayden@gmail.com>
2020-05-29 15:31:12 -0400

    Added project

---
+++ Snake/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Snake
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblScore = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lblHighScore = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.pictureBox.Location = new System.Drawing.Point(24, 14);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(620, 632);
+			this.pictureBox.TabIndex = 0;
+			this.pictureBox.TabStop = false;
+			this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(650, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Score:";
+			// 
+			// lblScore
+			// 
+			this.lblScore.AutoSize = true;
+			this.lblScore.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblScore.Location = new System.Drawing.Point(730, 13);
+			this.lblScore.Name = "lblScore";
+			this.lblScore.Size = new System.Drawing.Size(22, 23);
+			this.lblScore.TabIndex = 2;
+			this.lblScore.Text = "0";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(650, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(117, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Highscore:";
+			// 
+			// lblHighScore
+			// 
+			this.lblHighScore.AutoSize = true;
+			this.lblHighScore.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblHighScore.Location = new System.Drawing.Point(773, 38);
+			this.lblHighScore.Name = "lblHighScore";
+			this.lblHighScore.Size = new System.Drawing.Size(22, 23);
+			this.lblHighScore.TabIndex = 4;
+			this.lblHighScore.Text = "0";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(883, 661);
+			this.Controls.Add(this.lblHighScore);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lblScore);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Snake";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblScore;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lblHighScore;
+	}
+}
+
---
+++ Snake/MainForm.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Input;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class MainForm : Form
+    {
+		private List<Circle> snake = new List<Circle>();
+        private Circle food = new Circle();
+		private Random random = new Random();
+		private Direction lastDirection = Direction.Left;
+		private Direction direction = Direction.Left;
+		private int score = 0;
+		private bool pause = false;
+		private int highScore = 0;
+		private SoundPlayer eatSound;
+		private SoundPlayer deathSound;
+
+		public MainForm()
+        {	
+			string temp;
+
+			InitializeComponent();
+
+			try
+			{
+				eatSound = new SoundPlayer("C:\\Program Files (x86)\\Snake\\eat.wav");
+				deathSound = new SoundPlayer("C:\\Program Files (x86)\\Snake\\death.wav");
+
+				temp = File.ReadAllText(string.Format("C:\\Users\\{0}\\AppData\\Local\\Temp\\snakehighscore.txt", Environment.UserName));
+				if (temp != "")
+				{
+					lblHighScore.Text = temp;
+					highScore = int.Parse(temp);
+				}
+			}
+			catch { }
+
+			timer.Tick += UpdateScreen;
+			timer.Start();
+
+			StartGame();
+        }
+
+		private void StartGame()
+		{
+			Circle head = new Circle();
+
+			snake.Clear();
+
+			head.X = random.Next(0, pictureBox.Size.Width / 16);
+			head.Y = random.Next(0, pictureBox.Size.Height / 16);
+			snake.Add(head);
+
+			GenerateFood();
+		}
+
+		private void GenerateFood()
+		{
+			food.X = random.Next(0, pictureBox.Size.Width / 16);
+			food.Y = random.Next(0, pictureBox.Size.Height / 16);
+			for (int i = 0; i < snake.Count; i++)
+			{
+				if(snake[i].X == food.X && snake[i].Y == food.Y)
+				{
+					GenerateFood();
+				}
+			}
+		}
+
+		private void UpdateScreen(object sender, EventArgs e)
+		{ 
+			if(Keyboard.IsKeyDown(Key.P))
+			{
+				if(pause)
+				{
+					pause = false;
+				}
+				else
+				{
+					pause = true;
+				}
+			}
+
+			if(!pause)
+			{
+				MoveSnake();
+				pictureBox.Invalidate();
+			}
+		}
+
+		private void MoveSnake()
+		{
+			for (int i = snake.Count - 1; i >= 0; i--)
+			{
+				if(i == 0)
+				{
+					if (direction == Direction.Left && lastDirection != Direction.Right)
+					{
+						lastDirection = Direction.Left;
+						snake[i].X--;
+					}
+					else if (direction == Direction.Right && lastDirection != Direction.Left)
+					{
+						lastDirection = Direction.Right;
+						snake[i].X++;
+					}
+					else if (direction == Direction.Up && lastDirection != Direction.Down)
+					{
+						lastDirection = Direction.Up;
+						snake[i].Y--;
+					}
+					else if (direction == Direction.Down && lastDirection != Direction.Up)
+					{
+						lastDirection = Direction.Down;
+						snake[i].Y++;
+					}
+					else
+					{
+						lastDirection = Direction.Left;
+						snake[i].X--;
+					}
+
+					if (snake[i].X < 0 || snake[i].Y < 0 || snake[i].X >= (pictureBox.Size.Width / 16) || snake[i].Y >= (pictureBox.Size.Height / 16))
+					{
+						try
+						{
+							deathSound.Play();
+						}
+						catch { }
+						
+						StartGame();
+						if(score > highScore)
+						{
+							highScore = score;
+							File.WriteAllText(string.Format("C:\\Users\\{0}\\AppData\\Local\\Temp\\snakehighscore.txt", Environment.UserName), highScore.ToString());
+						}
+						score = 0;
+						lblScore.Text = score.ToString();
+						lblHighScore.Text = highScore.ToString();
+					}
+
+					for (int j = 1; j < snake.Count; j++)
+					{
+						if (snake[i].X == snake[j].X && snake[i].Y == snake[j].Y)
+						{
+							try
+							{
+								deathSound.Play();
+							}
+							catch { }
+							
+							StartGame();
+							if (score > highScore)
+							{
+								highScore = score;
+								File.WriteAllText(string.Format("C:\\Users\\{0}\\AppData\\Local\\Temp\\snakehighscore.txt", Environment.UserName), highScore.ToString());
+							}
+							score = 0;
+							lblScore.Text = score.ToString();
+							lblHighScore.Text = highScore.ToString();
+						}
+					}
+
+					if ((snake[i].X == food.X) && (snake[i].Y == food.Y))
+					{
+						Eat();
+						GenerateFood();
+						score++;
+						if (score > highScore)
+						{
+							highScore = score;
+							File.WriteAllText(string.Format("C:\\Users\\{0}\\AppData\\Local\\Temp\\snakehighscore.txt", Environment.UserName), highScore.ToString());
+						}
+						lblScore.Text = score.ToString();
+						lblHighScore.Text = highScore.ToString();
+					}
+				}
+				else
+				{
+					snake[i].X = snake[i - 1].X;
+					snake[i].Y = snake[i - 1].Y;
+				}
+			}
+		}
+
+		private void Eat()
+		{
+			try
+			{
+				eatSound.Play();
+			}
+			catch { }
+			
+			snake.Add(new Circle(snake[snake.Count - 1].X, snake[snake.Count - 1].Y));
+		}
+
+		private void PictureBox_Paint(object sender, PaintEventArgs e)
+		{
+			Graphics graphics = e.Graphics;
+			Brush snakeColor;
+
+			for (int i = 0; i < snake.Count; i++)
+			{
+
+				if (i == 0)
+				{
+					snakeColor = Brushes.Black;
+				}
+				else
+				{
+					snakeColor = Brushes.DarkGray;
+				}
+				graphics.FillEllipse(snakeColor, new Rectangle(snake[i].X * 16, snake[i].Y * 16, 16, 16));
+				graphics.FillEllipse(Brushes.Red, new Rectangle(food.X * 16, food.Y * 16, 16, 16));
+			}
+		}
+
+		private void MainForm_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Up)
+			{
+				direction = Direction.Up;
+			}
+			else if(e.KeyCode == Keys.Down)
+			{
+				direction = Direction.Down;
+			}
+			else if(e.KeyCode == Keys.Right)
+			{
+				direction = Direction.Right;
+			}
+			else if(e.KeyCode == Keys.Left)
+			{
+				direction = Direction.Left;
+			}
+		}
+
+		enum Direction
+		{
+			Up,
+			Down,
+			Left,
+			Right
+		}
+	}
+}
